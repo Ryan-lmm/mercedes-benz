fed56ba30cde2343e294d3de479e88deea44c07b
Ryan-lmm <ryanlmm4@gmail.com>
2023-12-31 20:50:08 -0300

    optimizing

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -6,32 +6,26 @@ const Banner = () => (
             <p className={`text-[16px] pl-4 font-fredoka text-slate-500`}>Mercedes-Benz</p>
             <h1 className={`text-[52px] pl-4 font-varela text-white`}>The Future Are Here!</h1>
             <div className="w-full h-96 sm:flex justify-center">
-                <div className="sm:w-full">
+                <div className="xs:w-full xs-">
                     <img
                     src={car}
                     alt="car"
-                    width={100}
-                    height={100}
                     className="xs:w-full sm:border-r-2"
                     />
 
                 </div>  
-                <div className="sm:w-1/2 xs:h-1/2">
+                <div className="xs:w-full xs:h-full">
                     <img
                     src={carblack}
                     alt="carblack"
-                    width={100}
-                    height={100}
                     className="xs:w-full brightness-50"
                     />
                 </div>
 
-                <div className="sm:w-1/2 xs:mt-40 xs:h-1/2">
+                <div className="xs:w-full xs:mt-40 xs:h-1/2">
                     <img
                     src={carro}
                     alt="carro"
-                    width={100}
-                    height={100}
                     className="xs:w-full brightness-100"
                     />
                 </div>
